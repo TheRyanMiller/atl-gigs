dbfd94a77b99602a72eafbc1e0b7b77e11f3e80f
Ryan Miller <rmiller07@gmail.com>
2025-11-30 14:58:32 -0500

    Add minimal og-test endpoint for debugging

---
+++ atl-gigs/api/og-test.tsx
@@ -0,0 +1,31 @@
+import { ImageResponse } from "@vercel/og";
+
+export const config = {
+  runtime: "edge",
+};
+
+export default function handler() {
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          fontSize: 60,
+          color: "white",
+          background: "black",
+          width: "100%",
+          height: "100%",
+          display: "flex",
+          textAlign: "center",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        Hello World
+      </div>
+    ),
+    {
+      width: 1200,
+      height: 630,
+    }
+  );
+}
