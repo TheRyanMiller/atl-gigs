05b903e5f9c255f763856e199fe8d5943796b2cc
Ryan Miller <rmiller07@gmail.com>
2026-02-02 22:01:57 -0500

    Enlarge headline Spotify icon in modal

--- atl-gigs/src/components/EventModal.tsx
+++ atl-gigs/src/components/EventModal.tsx
@@ -139,7 +139,7 @@ export default function EventModal({ event, onClose }: EventModalProps) {
                             className="text-teal-400 hover:text-teal-300"
                             aria-label="Open Spotify artist"
                           >
-                            <FontAwesomeIcon icon={faSpotify} className="w-3.5 h-3.5 ml-1.5 relative -top-0.5" />
+                            <FontAwesomeIcon icon={faSpotify} className="w-4 h-4 ml-2 relative -top-1" />
                           </a>
                         )}
                       </span>
